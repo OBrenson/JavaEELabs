c14d3facc3a694d21831eb772450353f0dfb60ec
OBreneson <oleg-brenyov@yandex.ru>
2022-09-23 08:53:40 +0400

    3 лаба

--- lab3/src/com/lab3/Main.java
+++ lab3/src/com/lab3/Main.java
@@ -5,11 +5,16 @@ import com.lab1.vehicles.Car;
 import com.lab1.vehicles.MotorbikeHandler;
 import com.lab1.vehicles.Vehicle;
 import com.lab3.threads.executors.NamesExecsPrinter;
+import com.lab3.threads.queue.NamesPrinterQueue;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 public class Main {
+
+    private static final String PATH_PREFIX = "C:\\Users\\PC\\Desktop\\labs\\JavaEELabs\\lab3\\resources\\";
 
     public static void main(String[] args) throws DuplicateModelNameException, InterruptedException {
 
@@ -45,23 +50,36 @@ public class Main {
 //        ss1.start();
 //        ss2.start();
 
-        System.out.println("------executors-------");
-        Vehicle exec1 = generateVehicle("exec1", 100, true);
-        Vehicle exec2 = generateVehicle("exec2", 1000, true);
-        Vehicle exec3 = generateVehicle("exec3", 200, true);
-        Vehicle exec4 = generateVehicle("exec4", 300, true);
+//        System.out.println("------executors-------");
+//        Vehicle exec1 = generateVehicle("exec1", 100, true);
+//        Vehicle exec2 = generateVehicle("exec2", 1000, true);
+//        Vehicle exec3 = generateVehicle("exec3", 200, true);
+//        Vehicle exec4 = generateVehicle("exec4", 300, true);
+//
+//        NamesExecsPrinter execsPrinter1 = new NamesExecsPrinter(exec1);
+//        NamesExecsPrinter execsPrinter2 = new NamesExecsPrinter(exec2);
+//        NamesExecsPrinter execsPrinter3 = new NamesExecsPrinter(exec3);
+//        NamesExecsPrinter execsPrinter4 = new NamesExecsPrinter(exec4);
+//
+//        ExecutorService executor = Executors.newFixedThreadPool(2);
+//        executor.execute(execsPrinter1);
+//        executor.execute(execsPrinter2);
+//        executor.execute(execsPrinter3);
+//        executor.execute(execsPrinter4);
+//        executor.shutdown();
 
-        NamesExecsPrinter execsPrinter1 = new NamesExecsPrinter(exec1);
-        NamesExecsPrinter execsPrinter2 = new NamesExecsPrinter(exec2);
-        NamesExecsPrinter execsPrinter3 = new NamesExecsPrinter(exec3);
-        NamesExecsPrinter execsPrinter4 = new NamesExecsPrinter(exec4);
-
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.execute(execsPrinter1);
-        executor.execute(execsPrinter2);
-        executor.execute(execsPrinter3);
-        executor.execute(execsPrinter4);
-        executor.shutdown();
+        System.out.println("------queue-------");
+        String[] paths = new String[5];
+        IntStream.range(0,5).forEachOrdered(i -> paths[i] = String.format("%scar%d.txt", PATH_PREFIX, i + 1));
+        ArrayBlockingQueue<Vehicle> queue = new ArrayBlockingQueue<>(1);
+        for (String path : paths) {
+            new Thread(new NamesPrinterQueue(queue, path)).start();
+        }
+        int ind = 0;
+        while (ind < 5) {
+            System.out.println(queue.take().getBrand());
+            ind++;
+        }
     }
 
     public static Vehicle generateVehicle(String prefix, int num, boolean isCar) throws DuplicateModelNameException {
---
+++ lab3/src/com/lab3/threads/queue/NamesPrinterQueue.java
@@ -0,0 +1,33 @@
+package com.lab3.threads.queue;
+
+import com.lab1.vehicles.Car;
+import com.lab1.vehicles.Vehicle;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+
+public class NamesPrinterQueue implements Runnable {
+
+    private final BlockingQueue<Vehicle> queue;
+    private final String path;
+
+    public NamesPrinterQueue(BlockingQueue<Vehicle> queue, String path) {
+        this.queue = queue;
+        this.path = path;
+    }
+
+    @Override
+    public void run() {
+        try (
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+        ){
+            String brand = br.readLine();
+            queue.put(new Car(brand, 0));
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
